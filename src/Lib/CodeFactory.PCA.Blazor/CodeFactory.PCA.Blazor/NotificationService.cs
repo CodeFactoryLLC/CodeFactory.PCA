31ef6a445c7212c2c7d5e5a6600aaa9d4c34cf60
Stephen Giffin <Stephen.Giffin@lifecycledelivery.com>
2024-06-03 10:25:39 -0700

    Updated documentation

--- src/Lib/CodeFactory.PCA.Blazor/CodeFactory.PCA.Blazor/NotificationService.cs
+++ src/Lib/CodeFactory.PCA.Blazor/CodeFactory.PCA.Blazor/NotificationService.cs
@@ -10,26 +10,31 @@ namespace CodeFactory.PCA.Blazor
 
         private INotificationSubmission _notificationComponent = null;
 
+        ///<inheritdoc/>
         public async Task ErrorNotificationAsync(string message, int timeout = 5000)
         {
             await(_notificationComponent?.ErrorNotificationAsync(message, timeout) ?? Task.CompletedTask);
         }
 
+        ///<inheritdoc/>
         public async Task InformationNotificationAsync(string message, int timeout = 5000)
         {
             await(_notificationComponent?.InformationNotificationAsync(message, timeout) ?? Task.CompletedTask);
         }
 
+        ///<inheritdoc/>
         public void RegisterNotificationProvider(CentralNotificationComponentBase provider)
         {
             _notificationComponent = provider;
         }
 
+        ///<inheritdoc/>
         public async Task SuccessNotificationAsync(string message, int timeout = 5000)
         {
             await (_notificationComponent?.SuccessNotificationAsync(message, timeout) ?? Task.CompletedTask);
         }
 
+        ///<inheritdoc/>
         public async Task WarningNotificationAsync(string message, int timeout = 5000)
         {
             await(_notificationComponent?.WarningNotificationAsync(message, timeout) ?? Task.CompletedTask);
